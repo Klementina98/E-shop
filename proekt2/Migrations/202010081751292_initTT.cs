7f8b92f87920ab3e295490eacfada46844c758b9
Klementina98 <klementinagorgieva7@hotmail.com>
2021-01-12 16:59:51 +0100

    MyProject

---
+++ proekt2/Migrations/202010081751292_initTT.cs
@@ -0,0 +1,28 @@
+﻿namespace proekt2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initTT : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.ArtWorks", "artist_Id", "dbo.Artists");
+            DropIndex("dbo.ArtWorks", new[] { "artist_Id" });
+            AddColumn("dbo.ArtWorks", "artist_Id1", c => c.Int());
+            AlterColumn("dbo.ArtWorks", "artist_Id", c => c.Int(nullable: false));
+            CreateIndex("dbo.ArtWorks", "artist_Id1");
+            AddForeignKey("dbo.ArtWorks", "artist_Id1", "dbo.Artists", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ArtWorks", "artist_Id1", "dbo.Artists");
+            DropIndex("dbo.ArtWorks", new[] { "artist_Id1" });
+            AlterColumn("dbo.ArtWorks", "artist_Id", c => c.Int());
+            DropColumn("dbo.ArtWorks", "artist_Id1");
+            CreateIndex("dbo.ArtWorks", "artist_Id");
+            AddForeignKey("dbo.ArtWorks", "artist_Id", "dbo.Artists", "Id");
+        }
+    }
+}
---
+++ proekt2/Models/ArtWorks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace proekt.Models
+{
+    public class ArtWorks
+    {
+        [Key]
+        public int Id { get; set; }
+        public String name { get; set; }
+        public String img { get; set; }
+        public int price { get; set; }
+        public int year { get; set; }
+        public int artist_Id { get; set; }
+        public virtual Artist artist { get; set; }
+
+        
+
+
+    }
+}
